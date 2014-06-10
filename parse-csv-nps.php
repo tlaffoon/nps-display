7379ba46677484b36adbb648a97588922f94e3a4
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-06-10 10:34:37 -0500

    Refactored code to utilize newly created ProcessCSV class.

--- parse-csv-nps.php
+++ parse-csv-nps.php
@@ -1,92 +1,22 @@
 <?php
 // parse-csv.php
 
-$filename = '../data/week5.csv';
+include('./ProcessCSV.php');
 
-function calcAVG($scores) {
+$objectOne = new ProcessCSV('../data/week5.csv');
 
-	foreach ($scores as $entries) {
-		foreach ($entries as $entry) {
-			$avg[] = $entries['Score'];
-		}
-	}
+$parsedData = $objectOne->parseCSV();
 
-	return number_format(array_sum($avg) / count($avg), 1, '.', '');
-}
+$entries = $objectOne->makeEntries($parsedData);
 
-function calcResponseRate($parsed, $entries) {
-	$rate = (count($entries) / count($parsed)) * 100;
-	return number_format($rate);
-}
+$averageScore = $objectOne->calcAVG($entries);
 
-function parseCSV($filename, $delimiter=',')
-{
-    if(!file_exists($filename) || !is_readable($filename))
-        return FALSE;
+$rate = $objectOne->calcResponseRate($parsedData, $entries);
 
-    $header = NULL;
-    $data = array();
-    if (($handle = fopen($filename, 'r')) !== FALSE)
-    {
-        while (($row = fgetcsv($handle, 1000, $delimiter)) !== FALSE)
-        {
-            if(!$header)
-                $header = $row;
-            else
-                $data[] = array_combine($header, $row);
-        }
-        fclose($handle);
-    }
-
-    // Drop first line in csv
-    unset($data[0]);
-    return $data;
-}
-
-function makeEntries($parsed) {
-
-	foreach ($parsed as $entry) {
-		$formatted = [];
-		$nonrespondents = [];
-
-		if (!empty($entry['Score'])) {
-			
-			foreach ($entry as $key => $value) {
-
-				if (isset($entry['Score']) && !empty($entry['Score'])) {
-					$formatted['Score'] = $entry['Score'];
-				}
-			
-				if (isset($entry['First Name'])) {
-					$formatted['First Name'] = $entry['First Name'];
-				}
-
-				if (isset($entry['Last Name'])) {
-					$formatted['Last Name'] = $entry['Last Name'];
-				}
-
-				if (isset($entry['Email Address'])) {
-					$formatted['Email Address'] = $entry['Email Address'];
-				}
-			
-				if (isset($entry['Comment'])) {
-					$formatted['Comment'] = $entry['Comment'];
-				}
-			}
-		}  
-
-		if (!empty($formatted)) {
-			$entries[] = $formatted;
-		}	
-	} 
-
-	return $entries;
-}
-
-$parsed = parseCSV($filename);
-$entries = makeEntries($parsed);
-$averageScore = calcAVG($entries);
-$rate = calcResponseRate($parsed, $entries);
+// $parsed = parseCSV($filename);
+// $entries = makeEntries($parsed);
+// $averageScore = calcAVG($entries);
+// $rate = calcResponseRate($parsed, $entries);
 
 ?>
 
@@ -107,7 +37,7 @@ $rate = calcResponseRate($parsed, $entries);
 	</div>
 </div>
 
-<div class="container">
+<div class="container-fluid">
 	<h4 class="week"> Week 5 </h4>
 </div>
 
